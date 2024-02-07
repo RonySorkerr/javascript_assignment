ac37d2a65ad34004d5091445ad4a668edd7f85f1
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-07 19:17:08 +0600

    going to second one

---
+++ problem2.js
@@ -0,0 +1,6 @@
+// takes a string input
+// check is the last char is A, y, i ,e ,o, w
+// if these char is available at the end of name returt the result
+// if condition matches return "Good Name"
+// if condition does not match return "Bad Name"
+// BONUS ** other data type will return 'invalid'
